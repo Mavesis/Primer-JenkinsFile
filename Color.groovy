9fa676cbbe1c79a1488da9fcaf84daaeb363d74f
mvelazquez <mvelazquez@excentia.es>
2018-08-29 12:49:59 +0200

    añadido Color.groovy

--- Color.groovy
+++ Color.groovy
@@ -1,7 +1,7 @@
 // This shows a simple build wrapper example, using the AnsiColor plugin.
 node {
     // This displays colors using the 'xterm' ansi color map.
-   // ansiColor('xterm') 
+        ansiColor('xterm') 
         // Just some echoes to show the ANSI color.
         stage "\u001BPrimera Fase \u001B[0m Now not"
             echo'Hola mundo'
