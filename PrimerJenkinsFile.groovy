e8d01b1f154206db5a63f7e3d0ab7c93da6e59cd
mvelazquez <mvelazquez@excentia.es>
2018-08-29 13:44:16 +0200

    modificado PrimerJenkinsFile.groovy construir un proyecto maven

--- PrimerJenkinsFile.groovy
+++ PrimerJenkinsFile.groovy
@@ -1,17 +1,24 @@
 
 pipeline {
     agent any
+    tools{
+        maven 'maven3.5.0'
+    }
     stages { 
         stage('Primera fase') {
             steps {
                 echo 'Este es mi primer JenkinsFile'
             }
         }
-        stage('Segunda fase'){
+        stage('Segunda fase -> DESCARGA'){
             steps{
                 echo' Ahora vamos a descargar un proyecto MAVEN'
                 git 'https://github.com/jitpack/maven-simple.git'
 
+            }
+        } stage('Tercera fase -> CONSTRUCCION'){
+            steps{
+                sh 'mvn clean install'
             }
         }
     }
